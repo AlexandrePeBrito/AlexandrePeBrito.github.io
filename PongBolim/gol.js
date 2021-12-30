daf2cd46e6f26607145ab2c9dba39ee667355539
AlexandrePeBrito <alexandre_pereirabrito@yahoo.com.br>
2021-12-30 14:26:19 -0300

    pongbolim

---
+++ PongBolim/gol.js
@@ -0,0 +1,11 @@
+//GOL
+let meuGol=[0,130]
+let enemyGol=[595,130]
+
+let caracteristicasGol=[5,120]
+//caracteristicasGol[0] ->comprimentoGol
+//caracteristicasGol[1] ->alturaGol
+
+function criarGol(x,y){
+  rect(x,y,caracteristicasGol[0],caracteristicasGol[1]);
+}
